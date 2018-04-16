665eb0bc6356a7b3489ed29a3bf59235883948a5
real-it <fjeanie22@gmail.com>
2018-04-16 20:01:13 +0300

    Homework

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,37 @@
-var a = 1;
+do {
+    do {
+        var number1 = +prompt("Первое число?");
+        } while (isNaN(number1));
+
+    do {
+        var number2 = +prompt("Второе число?");
+        } while (isNaN(number2));
+
+    // do {
+    //     var action = prompt("Какое действие выполнить? Возможные значения + - / * ");
+    //     } while (action !== "+");
+
+var action = prompt("Какое действие выполнить? Возможные значения + - / * ");
+var res = confirm("Вы хотите увидеть результат?");
+} while (res != true) ;
+
+if (res == true) {
+    switch (action) {
+        case "+" :
+            alert(number1 + number2);
+            break;
+        case "-" :
+            alert(number1 - number2);
+            break;
+        case "/" :
+            if (number2 !== 0) {
+            alert(number1 / number2);
+            break;
+            } else {
+            alert("не делится на ноль");}
+            break;
+        case "*" :
+            alert(number1 * number2);
+            break;
+        default :
+            alert("не то");
