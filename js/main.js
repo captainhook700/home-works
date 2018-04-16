c94c41c9b33a4f7c01e1deb15e5e2ef756f2fe00
Alexandr1818 <samsungi777s@gmail.com>
2018-04-16 18:54:48 +0300

    HomeWorks 2 in progress

---
+++ js/main.js
@@ -0,0 +1,132 @@
+
+
+
+sum('', '')
+
+function sum(a, b) {
+	var a = prompt("введите цыфру", '')
+		if (a < 0 || b < 0) {
+		alert("pls check number")
+		
+	}
+	else {
+		true;
+	}
+	
+	var b = prompt("введите цыфру два", '')
+		if (a < 0 || b < 0) {
+		alert("pls check number")
+		return false;
+	}
+	else {
+		true;
+	}
+	var res = prompt("выберете действие", '')
+	if (a < 0 || b < 0) {
+		alert("pls check number")
+	console.log(a + b)
+} else  {
+	return false;
+}
+
+};
+
+
+
+	
+	// function sum(a, b) {
+		 
+	// 	};
+
+	// 		prompt("Введите цыфру", '');
+	// 		var a = ( [0 - 9] +);
+	// 		if ( a == false ) {
+	// 			// if ( [0 - 9] +) {
+ //    			alert("Вы не ввели цыфру");
+
+ //    			// return ;
+ // 				prompt("Введите цыфру", '');
+
+ // 			}
+ 				    		
+	// 	var b = prompt("Введите вторую цыфру", '');
+
+	// 	var choose = prompt("Выберете действие", '');
+	// 		if (choose == "+") {
+	// 			sum = (a + b);
+	// 			console.log(a + b);
+	// 		};
+
+
+		// console.log('a + b');
+
+		// result = a + b;
+	
+
+
+
+// function count(num1, num2, mark) {
+//   switch (mark) {
+//     case "*":
+//       return num1 * num2;
+//     case "+":
+//       return num1 + num2;
+//     case "-":
+//       return num1 - num2;
+//     case "/":
+//       if (num2 !== 0) {
+//         return num1 / num2;
+//       } else {
+//         return "не делится на нуль";
+//       }
+//   }
+// }
+		
+// 		prompt("Введите цыфру", '');
+// 		// num1 = (/ [0 - 9]/ + )
+// 		if (num1 == true) {
+// 			alert("еще")
+// 		} 
+// 		else {
+//         alert(" не делится на нуль")
+// 			}
+
+
+// function calculator() {
+// var result;
+// var a = value;
+// var b = value;
+// switch(action) {
+// case "+":
+// result = a + b;
+// break;
+// case "-":
+// result = a - b;
+// break;
+// case "*":
+// result = a * b;
+// break;
+// case "/":
+// if ((a == 0) || (b == 0)) {
+// alert('делить на ноль нельзя');
+// }
+// else {
+// res = a/b;
+// }
+// break;
+//   }
+// }
+// var function = function calculator(action){
+// prompt("Введите цыфру");
+// var a = + typeof(number);
+//  if(a === 0 || a < 0){
+//  	alert("делить на ноль нельзя")
+//    console.log('a')
+//  } 
+//  prompt("Введите вторую цыфру");
+//  var b = + typeof(number);
+//   if(b > 0) {
+//   	alert('Показать результат?')
+//   	console.log('resault')
+//   }
+//  }
