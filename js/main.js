245926332b7e898e734f3c17bd2b5b1b36936269
Alexandr1818 <samsungi777s@gmail.com>
2018-04-18 13:03:42 +0300

    HomeWorks 02 in progress

--- js/main.js
+++ js/main.js
@@ -1,132 +1,58 @@
-
-
-
-sum('', '')
-
-function sum(a, b) {
-	var a = prompt("введите цыфру", '')
-		if (a < 0 || b < 0) {
-		alert("pls check number")
+var num1,
+	num2,
+	result,
+	operation;
+ /*есть переменные(которые выше) и должны
+	быть функции на каждое действие,
+	и я так понимаю что должна быть одна функция
+	в которой все остальные функции
+	выполняют свою работу, как то так? */
+count();
+function count() {                 			 //тут запросы на числа и
+								   			 // и скорей всего тут же можно и 
+	var num1 = prompt('choose number1', '')  // проверить числа
 		
-	}
-	else {
-		true;
-	}
-	
-	var b = prompt("введите цыфру два", '')
-		if (a < 0 || b < 0) {
-		alert("pls check number")
-		return false;
-	}
-	else {
-		true;
-	}
-	var res = prompt("выберете действие", '')
-	if (a < 0 || b < 0) {
-		alert("pls check number")
-	console.log(a + b)
-} else  {
-	return false;
+	if (num1 != isNaN) {
+		alert('NO NUMBER')
+	} else if {
+		return (count);
+	} else if {						
+		var num2 = prompt('choose number2', '') // незнаю можно ли тут дальше
+	}											//корректно описать num2  или
+												// нужно отдельную функию создавать?
 }
 
-};
+ function getnum() {    // функция определяет введенное первое 
+ 						//и второе число	
+ 							// тут мне не понятно как и чем забрать
+ 							// введенные чила
 
+ }
 
+ function operation() { //проверка + - * и её вычесление
+ 	switch (mark) {
+    case "*":
+      return num1 * num2;
+    case "+":                     //тут я не доконца разобрался с проверкой на 
+    							  //введенный оператор и опять таки чем и как его забрать
+      return num1 + num2;
+    case "-":
+      return num1 - num2;
+    case "/":
+      if (num2 !== 0) {
+        return num1 / num2;
+      } else {
+        return "не делится на нуль";
+      }
+  }
+}
+ }
 
-	
-	// function sum(a, b) {
-		 
-	// 	};
+ function result() { 	//результат
 
-	// 		prompt("Введите цыфру", '');
-	// 		var a = ( [0 - 9] +);
-	// 		if ( a == false ) {
-	// 			// if ( [0 - 9] +) {
- //    			alert("Вы не ввели цыфру");
+ }
 
- //    			// return ;
- // 				prompt("Введите цыфру", '');
-
- // 			}
- 				    		
-	// 	var b = prompt("Введите вторую цыфру", '');
-
-	// 	var choose = prompt("Выберете действие", '');
-	// 		if (choose == "+") {
-	// 			sum = (a + b);
-	// 			console.log(a + b);
-	// 		};
-
-
-		// console.log('a + b');
-
-		// result = a + b;
-	
-
-
-
-// function count(num1, num2, mark) {
-//   switch (mark) {
-//     case "*":
-//       return num1 * num2;
-//     case "+":
-//       return num1 + num2;
-//     case "-":
-//       return num1 - num2;
-//     case "/":
-//       if (num2 !== 0) {
-//         return num1 / num2;
-//       } else {
-//         return "не делится на нуль";
-//       }
-//   }
-// }
-		
-// 		prompt("Введите цыфру", '');
-// 		// num1 = (/ [0 - 9]/ + )
-// 		if (num1 == true) {
-// 			alert("еще")
-// 		} 
-// 		else {
-//         alert(" не делится на нуль")
-// 			}
-
-
-// function calculator() {
-// var result;
-// var a = value;
-// var b = value;
-// switch(action) {
-// case "+":
-// result = a + b;
-// break;
-// case "-":
-// result = a - b;
-// break;
-// case "*":
-// result = a * b;
-// break;
-// case "/":
-// if ((a == 0) || (b == 0)) {
-// alert('делить на ноль нельзя');
-// }
-// else {
-// res = a/b;
-// }
-// break;
-//   }
-// }
-// var function = function calculator(action){
-// prompt("Введите цыфру");
-// var a = + typeof(number);
-//  if(a === 0 || a < 0){
-//  	alert("делить на ноль нельзя")
-//    console.log('a')
-//  } 
-//  prompt("Введите вторую цыфру");
-//  var b = + typeof(number);
-//   if(b > 0) {
-//   	alert('Показать результат?')
-//   	console.log('resault')
-//   }
-//  }
+ 
+ function checknum () {  // проверка на ввод числа или буквы
+ 	 
+ }
