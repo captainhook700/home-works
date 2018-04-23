202a584f35c90cf68dc97c79b24a8fd797d8c703
Anton <hulkofsky@gmail.com>
2018-04-23 18:44:17 +0300

    Calc v2.0

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,90 +1,43 @@
-function modal() {
-    var firstNum = prompt("Enter the number");
+var calc = {
+sum: function() {
+var totalSum = 0;
 
-    if (firstNum === "") {
-        alert("Is empty!");
-        return modal();
-    } else if (!!firstNum === false) {
-      return;
-    }
-
-    switch(isNaN(firstNum)) {
-        case true:
-            alert("Not a number");
-            modal();
-            break;
-        case false:
-            var secondModal = function() {
-                var secondNum = prompt("Enter the second number");
-
-                if (secondNum === "") {
-                    alert("Is empty!");
-                    return modal();
-                } else if (!!secondNum === false) {
-                  return;
-                }
-
-                switch(isNaN(secondNum)) {
-                    case true:
-                        alert("Not a number");
-                        modal();
-                        break;
-                    case false:
-                        var operation = function() {
-                            var operator = prompt("Enter operator");
-                            var result = confirm("Display result?");
-
-                            switch(operator || !!operator) {
-                                case false:
-                                  return;
-                                  break;
-
-                                case '+':
-                                    if (result === true) {
-                                        alert((+firstNum)+(+secondNum));
-                                    } else {
-                                        modal();
-                                    }
-                                    break;
-
-                                case '-':
-                                    if (result === true) {
-                                        alert((+firstNum)-(+secondNum));
-                                    } else {
-                                        modal();
-                                    }
-                                    break;
-
-                                case '*':
-                                    if (result === true) {
-                                        alert((+firstNum)*(+secondNum));
-                                    } else {
-                                        modal();
-                                    }
-                                    break;
-
-                                case '/':
-                                    if (secondNum === "0") {
-                                        alert('Impossible');
-                                        modal();
-                                    } else if (result === true) {
-                                        alert((+firstNum)/(+secondNum));
-                                    } else if (result === false) {
-                                        modal();
-                                    }
-                                    break;
-                                default:
-                                    alert("Unknown operator");
-                                    modal();
-                            }
-                        }
-                        operation();
-                        break;
-                }
-            }
-            secondModal();
-            break;
-    }
+for (var i = 0; i < arguments.length; i++) {
+totalSum += arguments[i];
 }
+return totalSum;
+},
+mult: function() {
+var totalMult = 1;
 
-modal();
+for (var i = 0; i < arguments.length; i++) {
+totalMult *= arguments[i];
+}
+return totalMult;
+},
+div: function() {
+var totalDiv = arguments[0];
+
+for (var i = 1; i < arguments.length; i++) {
+totalDiv /= arguments[i];
+}
+return totalDiv;
+},
+sub: function() {
+var totalSub = arguments[0];
+
+for (var i = 1; i < arguments.length; i++) {
+totalSub -= arguments[i];
+}
+return totalSub;
+},
+exp: function() {
+var totalExp = arguments[0]**arguments[1];
+return totalExp;
+},
+fact: function() {
+return (arguments[0]) ? arguments[0] * this.fact(arguments[0] - 1) : 1;
+
+}
+}
+console.log(calc.sum(5,5));
