1bc8832ee99b8a30cecef30a90a199183b3fb077
JuliFed <fedorchenkojuli@gmail.com>
2018-04-24 10:32:55 +0300

    Events: addEventListner

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,19 @@
-var a = 1;
+var accordsClick = document.querySelector('#menu1').querySelectorAll('.accordion');
+for (var i=0; i<accordsClick.length; i++){
+	accordsClick[i].addEventListener('click',openPanel);
+}
+accordsClick = document.querySelector('#menu2').querySelectorAll('.accordion');
+for (var i=0; i<accordsClick.length; i++){
+	accordsClick[i].addEventListener('mouseenter',openPanel);
+}
+
+
+function openPanel() {
+	var allAccords = this.parentElement.querySelectorAll('.accordion');
+	for (var i=0; i<allAccords.length; i++){
+	 	allAccords[i].classList.remove('active');
+	 	allAccords[i].nextElementSibling.setAttribute('hidden',true);
+	}
+	this.classList.add('active');
+	this.nextElementSibling.removeAttribute('hidden');
+}
