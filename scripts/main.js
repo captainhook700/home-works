c774d0951f37be05f41173b78d89c8120f9d2bd9
JuliFed <fedorchenkojuli@gmail.com>
2018-04-25 12:06:12 +0300

    opening multiple panels at the same time

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,22 @@
 var accordsClick = document.querySelector('#menu1').querySelectorAll('.accordion');
-for (var i=0; i<accordsClick.length; i++){
+
+for (var i = 0; i < accordsClick.length; i++){
 	accordsClick[i].addEventListener('click',openPanel);
 }
+
 accordsClick = document.querySelector('#menu2').querySelectorAll('.accordion');
-for (var i=0; i<accordsClick.length; i++){
+
+for (var i = 0; i < accordsClick.length; i++){
 	accordsClick[i].addEventListener('mouseenter',openPanel);
 }
 
+accordsClick = document.querySelector('#menu3').querySelectorAll('.accordion');
 
-function openPanel() {
+for (var i = 0; i < accordsClick.length; i++){
+	accordsClick[i].addEventListener('click',openPanelMoreOpened);
+}
+
+function openPanel(event) {
 	var allAccords = this.parentElement.querySelectorAll('.accordion');
 	for (var i=0; i<allAccords.length; i++){
 	 	allAccords[i].classList.remove('active');
@@ -16,4 +24,13 @@ function openPanel() {
 	}
 	this.classList.add('active');
 	this.nextElementSibling.removeAttribute('hidden');
+}
+
+function openPanelMoreOpened(event) {
+	this.classList.toggle('active');
+	if (this.nextElementSibling.hasAttribute('hidden')) {
+		this.nextElementSibling.removeAttribute('hidden');
+	}else{
+		this.nextElementSibling.setAttribute('hidden',true);
+	}
 }
