7ad7132ce0c11fa2dfdb855cf197ebaaf7000224
yarikroterdamskiygmailcom <37020321+yarikroterdamskiygmailcom@users.noreply.github.com>
2018-04-25 18:33:05 +0300

    lesson5

---
+++ js.js
@@ -0,0 +1,46 @@
+var main = document.getElementById('click');
+var title = document.getElementById('hidden')
+    
+	main.onclick = function() {
+    title.style.display = "block";
+    }
+	document.body.onmouseover = function() {
+    title.style.display = "none";
+	title1.style.display = "none";
+	title2.style.display = "none";
+	title3.style.display = "none";
+	title4.style.display = "none";
+    }
+	
+	
+	
+var main1 = document.getElementById('click1');
+var title1 = document.getElementById('hidden1')
+
+	main1.onclick = function() {
+	title1.style.display = "block";
+	}
+	
+	
+var main2 = document.getElementById('click2');
+var title2 = document.getElementById('hidden2')
+
+	main2.onclick = function() {
+	title2.style.display = "block";
+	}
+	
+var main3 = document.getElementById('click3');
+var title3 = document.getElementById('hidden3')
+
+	main3.onclick = function() {
+	title3.style.display = "block";
+	}
+	
+
+var main4 = document.getElementById('click4');
+var title4 = document.getElementById('hidden4')
+
+	main4.onclick = function() {
+	title4.style.display = "block";
+	}
+	
