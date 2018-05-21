ec705b2bdcf6f3576e648b05a4324c5a2218502a
JuliFed <fedorchenkojuli@gmail.com>
2018-05-21 20:01:33 +0300

    Added captcha in lesson

--- assets/script.js
+++ assets/script.js
@@ -1,32 +1,40 @@
 $(document).ready(function(){
     $('#email').on('blur', emailValidation);
     
-    $('#password').on('blur', passwordValidation);
-    $('#password').on('blur', confirmValidation);
+    $('#password').on('keyup', passwordValidation);
+    $('#password').on('keyup', confirmValidation);
 
-    $('#confirm').on('blur', confirmValidation);
+    $('#confirm').on('keyup', confirmValidation);
 
     $('input[type="submit"]').prop('disabled', true);
     $('input[type="submit"]').on('click', submitForm);
-    
+
+    var firstNum = Math.floor(Math.random() * (10 - 1)) + 1;
+    var secondNum = Math.floor(Math.random() * (10 - 1)) + 1;
+    resultCaptcha = firstNum+secondNum;
+    $('#captcha').attr('placeholder',firstNum + '+' + secondNum);
+    $('#captcha').on('keyup', captchaValidation);
 
 });
-
+var resultCaptcha = 0;
 var Invalid = {
     email: 0,
     password: 0,
-    confirm: 0
+    confirm: 0,
+    captcha: 0
 }
 
 var Validation = {
     status : {
         0: "OK",
+        1: "Captcha not valid",
         2: "Пустое поле email",
         3: "Пустое поле password",
         4: "Пустое поле confirm password",
         5: "Пароль должен быть длиннее 6 символов",
         6: "Пароль и подтверждение не совпадают",
-        7: "Введите корректный email"
+        7: "Введите корректный email",
+        8: "Введите капчу!"
 
     },
     email : function(input_email) {
@@ -39,6 +47,7 @@ var Validation = {
         Invalid['email'] = result;
         this.requirements(input_email, result);
     },
+
     password : function(input_password) {
         var pass = $(input_password).val();
         var result = 3;
@@ -48,6 +57,7 @@ var Validation = {
         Invalid['password'] = result;
         this.requirements(input_password, result);
     },
+
     confirm(pass, input_confirm) {
         var confirm = $(input_confirm).val()
         var result = 4;
@@ -56,6 +66,16 @@ var Validation = {
         }
         Invalid['confirm'] = result;
         this.requirements(input_confirm, result);
+    },
+
+    captcha : function(input_captcha){
+        var result = 8;
+        var captcha = $(input_captcha).val()
+        if (captcha != ""){
+            result = (+captcha == resultCaptcha) ? 0 : 1;
+        }
+        Invalid["captcha"] = result;
+        this.requirements(input_captcha, result);
     },
 
     requirements: function(input, error_text) {
@@ -69,6 +89,7 @@ var Validation = {
         }
         this.stateToSend();
     },
+
     stateToSend : function(){
         if (!Object.values(Invalid).reduce(function(sum, val) { return sum + val; })) {
             $('input[type="submit"]').removeAttr('disabled');
@@ -91,6 +112,10 @@ function confirmValidation(event) {
     var password = $("#password").val();
     var confirm = $("#confirm");
     Validation.confirm(password, confirm);
+}
+
+function captchaValidation(event) {
+    Validation.captcha(this);
 }
 
 function submitForm(event){
